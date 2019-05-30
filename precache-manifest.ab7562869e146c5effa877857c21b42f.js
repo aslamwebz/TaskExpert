85ca7354f3f225b532463fd220be0f267faba85e
EM <aslam4webz@gmail.com>
2019-05-30 21:12:23 +0530

    Updates

---
+++ precache-manifest.ab7562869e146c5effa877857c21b42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e4d5e0a7a7afdd1e584c85864e5f43",
+    "url": "/TaskExpert/index.html"
+  },
+  {
+    "revision": "fd5aed2d2547e0b2a955",
+    "url": "/TaskExpert/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "dae2bc9744b3d2700812",
+    "url": "/TaskExpert/static/css/main.44bfe4f6.chunk.css"
+  },
+  {
+    "revision": "fd5aed2d2547e0b2a955",
+    "url": "/TaskExpert/static/js/2.6b295edf.chunk.js"
+  },
+  {
+    "revision": "dae2bc9744b3d2700812",
+    "url": "/TaskExpert/static/js/main.2d90a963.chunk.js"
+  },
+  {
+    "revision": "827d2059c1cfee4045dc",
+    "url": "/TaskExpert/static/js/runtime~main.df08870f.js"
+  }
+]);
